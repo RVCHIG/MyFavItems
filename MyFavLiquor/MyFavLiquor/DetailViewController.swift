bad8f8a71937ec4a87a741edb9dea717c8bc927b
RVCHIG <43154394+RVCHIG@users.noreply.github.com>
2018-09-17 18:12:14 -0500

    Add files via upload

---
+++ MyFavLiquor/MyFavLiquor/DetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.swift
+//  MyFavLiquor
+//
+//  Created by cis290 on 9/17/18.
+//  Copyright © 2018 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        if detailItem != nil {
+            //********3) Update (6) lines of code below
+            if let photo = productImageView {
+                photo.image = UIImage(named:detailItem!)
+            }
+        }else{
+            productImageView.image = UIImage(named:"Main.png")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
